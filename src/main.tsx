ba157341f2c63f5a924c62c91db3c5264641481a
Muhammad Ammar <ammarshafique677@gmail.com>
2024-12-22 05:01:38 +0500

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import App from './App';
+import App from './App.tsx';
 import './index.css';
 
-// Get the root element
-const rootElement = document.getElementById('root');
-
-// Ensure root element exists
-if (!rootElement) {
-  throw new Error('Root element not found');
-}
-
-// Create root and render app
-const root = createRoot(rootElement);
-
-root.render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
+    <App />
   </StrictMode>
 );
