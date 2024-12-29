ce815a96eb8a1cc0de61e0d5a18d2ec72cc6b85a
Muhammad Ammar <ammarshafique677@gmail.com>
2024-12-29 19:41:04 +0500

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/is-ammar-1.github.io/', // Replace <your-repo-name> with your GitHub repository name
+  base: '/', // Changed from '/is-ammar-1.github.io/'
   build: {
     outDir: 'dist',},
   plugins: [react()],
