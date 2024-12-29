d26f60a75d69d41805d13803a419c240d78a16d6
Muhammad Ammar <ammarshafique677@gmail.com>
2024-12-29 19:47:25 +0500

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom'; // Change this from BrowserRouter
 import App from './App.tsx';
 import './index.css'; // <-- Import your global Tailwind + custom styles
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <HashRouter> {/* Change this from BrowserRouter */}
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Changed from '/is-ammar-1.github.io/'
+  base: '/is-ammar-1.github.io/',
   build: {
-    outDir: 'dist',},
+    outDir: 'dist',
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
